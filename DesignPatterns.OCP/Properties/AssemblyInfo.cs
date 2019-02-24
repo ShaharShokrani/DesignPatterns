63da2a13493f90d9ff9f10e18711b0ba8cfa794e
Shahar Shokrani <shokrano@gmail.com>
2019-02-24 17:44:41 +0200

    AssemblyTitle

--- DesignPatterns.OCP/Properties/AssemblyInfo.cs
+++ DesignPatterns.OCP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DesignPatterns.OCP")]
+[assembly: AssemblyTitle("DesignPatterns.SOLID.OCP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DesignPatterns.OCP")]
+[assembly: AssemblyProduct("DesignPatterns.SOLID.OCP")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
